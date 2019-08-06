ac92f24971bbd8de45fc73faa4d24d4afe15a2d4
Matt Stuhring <whereyouatmatt@gmail.com>
2019-08-05 20:45:42 -0700

    Implemented array-reverse

---
+++ code401challenges/ArrayReverse.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class ArrayReverse {
+
+  public static void main(String[] args) {
+    int[] toBeReversed = new int[]{1,2,3,4,5};
+    System.out.println(Arrays.toString((reverseArray(toBeReversed))));
+
+  }
+
+  public static int[] reverseArray(int[] arr) {
+    int[] reversedArr = new int[arr.length];
+
+    for (int i = 0; i < arr.length; i++) {
+      reversedArr[i] = arr[(arr.length - 1) - i];
+    }
+
+    return reversedArr;
+  }
+}
