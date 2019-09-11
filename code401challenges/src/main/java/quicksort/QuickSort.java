96ff21a2ec6d5103df41a4da6cdb2f47205c54b8
Matt Stuhring <whereyouatmatt@gmail.com>
2019-09-10 19:21:08 -0700

    Completed quick sort challenge

---
+++ code401challenges/src/main/java/quicksort/QuickSort.java
@@ -0,0 +1,47 @@
+package quicksort;
+
+public class QuickSort {
+
+    public static int[] quickSort(int[] input, int start, int end) {
+        if (end - start < 2) {
+            return null;
+        }
+
+        int pivotIndex = partition(input, start, end);
+
+        quickSort(input, start, pivotIndex);
+
+        for (int i = 0; i < input.length; i++) {
+            System.out.println(input[i]);
+        }
+
+        quickSort(input, pivotIndex + 1, end);
+
+        return input;
+    }
+
+    public static int partition(int[] input, int start, int end) {
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+
+            // NOTE: empty loop body
+            while (i < j && input[--j] >= pivot);
+            if (i < j) {
+                input[i] = input[j];
+            }
+
+            // NOTE: empty loop body
+            while (i < j && input[++i] <= pivot);
+            if (i < j) {
+                input[j] = input[i];
+            }
+        }
+
+        input[j] = pivot;
+        return j;
+    }
+
+}
